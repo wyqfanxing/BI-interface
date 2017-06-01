3ca4e1141fd679cb822e889255d36433d3044432
wyqfanxing <312945626.com>
2017-06-01 18:45:57 +0800

    提交

---
+++ src/main/java/com/wyq/jmeter/App.java
@@ -0,0 +1,13 @@
+package com.wyq.jmeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
